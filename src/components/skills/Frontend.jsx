8d898a3a14f99c51e9450061919370dac0fb1221
KongJen <supakorn.12.06.2003@gmail.com>
2024-08-26 08:49:33 +0700

    Add data project

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -89,6 +89,15 @@ const Frontend = () => {
               <span className="skills__level">intermediate</span>
             </div>
           </div>
+
+          <div className="skills__data">
+            <i className="bx bxs-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Java</h3>
+              <span className="skills__level">intermediate</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
